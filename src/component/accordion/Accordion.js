d9a7289d61a325c8424241017958f13623cfdb8b
parag <paragbramh@gmail.com>
2021-07-19 01:34:25 +0530

    adding files

---
+++ src/component/accordion/Accordion.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import {questions} from './api';
+import "./accordion.css";
+import MyAccordion from "./MyAccordion";
+
+const Accordion=()=>{
+
+    const [data,setData] = useState(questions);
+
+    return(
+        <>
+            <section className="main-div">
+        <h1>React Interview Questions</h1>
+           
+            {
+                data.map((curElem)=>{
+                    const {id,question,answer} = curElem;
+                    return <MyAccordion key={curElem.id} {...curElem}/>
+                })
+            }
+            </section>
+        </>
+    )
+}
+
+export default Accordion;
